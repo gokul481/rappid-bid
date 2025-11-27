ace2261f6b3777f26835eda231168717ea0be357
gokul481 <gr2232541@gmail.com>
2025-11-27 22:30:03 +0530

    Add files via upload

---
+++ admin/delete_auction.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$database   = "auction";
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
+
+if (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+    $conn->query("DELETE FROM add_product WHERE id=$id");
+}
+
+header("Location: admin/admindashboard.php");
+exit();
